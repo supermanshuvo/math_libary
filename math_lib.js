d188e66f85e0aac883b8090d6554f35ad79826fa
Rezaul Karim <shuvothesuperman@gmail.com>
2021-09-08 14:56:13 +0600

    Multiply funcitin is ready and console log for test also done and comment out the code

--- math_lib.js
+++ math_lib.js
@@ -17,7 +17,17 @@ let add = (...numbers)=>{
     let sum = 0;
     numbers.forEach(el => sum+=parseFloat(el));
     return sum;
-}
+};
 
 // Out put the value of add function
 console.log(add(1,2,3,3));
+
+// Multiplication Function
+let multiply = (...numbers)=>{
+    let multiplication = 1;
+    numbers.forEach(el => multiplication*=parseFloat(el));
+    return multiplication;
+};
+
+// Out put the value of Multiplication
+console.log(multiply(6));
