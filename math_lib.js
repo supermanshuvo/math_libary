5a1066aa3fe7c31b49847ef70e3fe26eadf4729b
supermanshuvo <shuvothesuperman@gmail.com>
2021-09-09 06:31:48 +0600

    reamove some unwanted value

--- math_lib.js
+++ math_lib.js
@@ -10,46 +10,37 @@
   # Division
   # Subtraction
 */
+// Create Class
+class Math_lib{
 
-// Addition Function
-let add = (...numbers)=>{
-    let result = 0;
-    numbers.forEach(el => result+=parseFloat(el));
-    return result;
-};
-
-// Out put the value of add function
-console.log(add(1,2,3,3));
-
-// Multiplication Function
-let multiply = (...numbers)=>{
-    let result = 1;
-    numbers.forEach(el => result*=parseFloat(el));
-    return result;
-};
-
-// Out put the value of Multiplication
-console.log(multiply(6,2,3));
-
-// Division Function 
-let div = (...numbers)=>{
-    let result = 0;
-    if(!numbers[1]){  //If arguments value is NaN, Undefined or 0  
-        numbers[1]=1;
+    // Addition Function
+    add (...numbers){
+        let result = 0;
+        numbers.forEach(el => result+=parseFloat(el));
+        return result;
     }
-    result = parseFloat(numbers[0])/parseFloat(numbers[1]);
-    return result;
-};
-
-// Out put the value of Division
-console.log(div(4,2))
-
-// Subtraction Function  
-let sub=(...numbers)=>{
-    let result=0;
-    result = parseFloat(numbers[0])-parseFloat(numbers[1]);
-    return result;
+    
+    // Multiplication Function
+    multiply(...numbers){
+        let result = 1;
+        numbers.forEach(el => result*=parseFloat(el));
+        return result;
+    }
+    
+    // Division Function 
+    div(...numbers){
+        let result = 0;
+        if(!numbers[1]){  //If arguments value is NaN, Undefined or 0  
+            numbers[1]=1;
+        }
+        result = parseFloat(numbers[0])/parseFloat(numbers[1]);
+        return result;
+    }
+    
+    // Subtraction Function  
+    sub(...numbers){
+        let result=0;
+        result = parseFloat(numbers[0])-parseFloat(numbers[1]);
+        return result;
+    }
 }
-
-// Out put the value of Subtraction
-console.log(sub(5,6));
