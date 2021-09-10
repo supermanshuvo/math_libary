c8f3fdf1fcfd64ca355a3b4877679375381f2907
supermanshuvo <shuvothesuperman@gmail.com>
2021-09-10 19:28:30 +0600

    fail:again

--- prototype_chaining/math_lib.js
+++ prototype_chaining/math_lib.js
@@ -10,7 +10,7 @@
   # Division
   # Subtraction
 */
-let Obj = {
+/* let Obj = {
     result: 0,
     add:function(...numbers){
         for(let i=0;i<arguments.length;i++){
@@ -39,4 +39,24 @@ let Obj = {
 
 };
 Obj.add(5, 10).multiply(6).subtract(10, 30, 8).divide(2);
-console.log(Obj.result);
+console.log(Obj.result); */
+
+
+/* function doSomething(){}
+doSomething.prototype.foo = 'Bar';
+let doSomeInstancing = new doSomething();
+doSomeInstancing.prop = "some value";
+console.log(doSomeInstancing); */
+/* function Math_lib(){}
+Math_lib.prototype.add = function(...numbers){
+    let result = 0;
+    for(let i;i<arguments.length;i++){
+        result+=numbers[i];
+    };
+    return this;
+}
+
+
+let result = Math_lib.add(1,2,3,4);
+console.log(result);
+*/
