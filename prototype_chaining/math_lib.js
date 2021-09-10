4c810e21cc7e9fe6c2e54bc9a0c8d0f2e7f08d00
supermanshuvo <shuvothesuperman@gmail.com>
2021-09-10 19:57:40 +0600

    Value test done

--- prototype_chaining/math_lib.js
+++ prototype_chaining/math_lib.js
@@ -10,9 +10,13 @@
   # Division
   # Subtraction
 */
-/* let Obj = {
-    result: 0,
-    add:function(...numbers){
+function Math_lib(){
+    let math = Object.create(Math_lib.prototype);
+    return math;
+}
+Math_lib.prototype={
+    result : 0,
+    add(...numbers){
         for(let i=0;i<arguments.length;i++){
             this.result+=numbers[i];
         }
@@ -37,26 +41,8 @@
         return this;
     }
 
-};
-Obj.add(5, 10).multiply(6).subtract(10, 30, 8).divide(2);
-console.log(Obj.result); */
-
-
-/* function doSomething(){}
-doSomething.prototype.foo = 'Bar';
-let doSomeInstancing = new doSomething();
-doSomeInstancing.prop = "some value";
-console.log(doSomeInstancing); */
-/* function Math_lib(){}
-Math_lib.prototype.add = function(...numbers){
-    let result = 0;
-    for(let i;i<arguments.length;i++){
-        result+=numbers[i];
-    };
-    return this;
 }
-
-
-let result = Math_lib.add(1,2,3,4);
-console.log(result);
-*/
+// console.log(Math_lib.prototype);
+const math = Math_lib();
+math.add(5, 10).multiply(6).subtract(10, 30, 8).divide(2);
+console.log(math.result);
