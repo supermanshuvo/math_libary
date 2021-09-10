3c55515d098f39325b991852b9ce451a550e220d
supermanshuvo <shuvothesuperman@gmail.com>
2021-09-10 17:32:52 +0600

    Setup prototype_chaining

---
+++ prototype_chaining/math_lib.js
@@ -0,0 +1,42 @@
+/* 
+  # Rezaul Karim
+   -------------
+   
+  ---------------
+   Maths Library
+  ---------------
+  # Addition
+  # Multiplication
+  # Division
+  # Subtraction
+*/
+let Obj = {
+    result: 0,
+    add:function(...numbers){
+        for(let i=0;i<arguments.length;i++){
+            this.result+=numbers[i];
+        }
+        return this;
+    },
+    multiply:function(...numbers){
+        for(let i=0;i<arguments.length;i++){
+            this.result*=numbers[i];
+        }
+        return this;
+    },
+    subtract:function(...numbers){
+        for(let i=0;i<arguments.length;i++){
+            this.result*=numbers[i];
+        }
+        return this;
+    },
+    divide:function(...numbers){
+        for(let i=0;i<arguments.length;i++){
+            this.result/=numbers[i];
+        }
+        return this;
+    }
+
+};
+Obj.add(5, 10).multiply(6).subtract(10, 30, 8).divide(2);
+console.log(Obj.result);
