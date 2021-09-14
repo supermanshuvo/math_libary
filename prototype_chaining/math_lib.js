ca45f82593d0079ae4b2e508bc41836e65fcc34b
Rezaul Karim <shuvothesuperman@gmail.com>
2021-09-14 13:40:44 +0600

    Prototype chaining is done

--- prototype_chaining/math_lib.js
+++ prototype_chaining/math_lib.js
@@ -11,36 +11,32 @@
   # Division
 */
 function Math_lib(){}
-/* Math_lib.prototype={
-    result : 0,
-    add(...numbers){
-        for(let i=0;i<arguments.length;i++){
-            this.result+=numbers[i];
-        }
-        return this;
-    },
-    multiply:function(...numbers){
-        for(let i=0;i<arguments.length;i++){
-            this.result*=numbers[i];
-        }
-        return this;
-    },
-    subtract:function(...numbers){
-        for(let i=0;i<arguments.length;i++){
-            this.result-=numbers[i];
-        }
-        return this;
-    },
-    divide:function(...numbers){
-        for(let i=0;i<arguments.length;i++){
-            this.result/=numbers[i];
-        }
-        return this;
+Math_lib.prototype.result = 0;
+Math_lib.prototype.add = function(...numbers){
+    for(let i=0;i<arguments.length;i++){
+        this.result+=numbers[i];
     }
-
-} */
-
-// console.log(Math_lib.prototype);
-// const math = Math_lib();
-// math.add(5, 10).multiply(6).subtract(10, 30, 8).divide(2);
+    return this;
+}
+Math_lib.prototype.multiply = function(...numbers){
+    for(let i=0;i<arguments.length;i++){
+        this.result*=numbers[i];
+    }
+    return this;
+}
+Math_lib.prototype.subtract = function(...numbers){
+    for(let i=0;i<arguments.length;i++){
+        this.result-=numbers[i];
+    }
+    return this;
+}
+Math_lib.prototype.divide = function(...numbers){
+    for(let i=0;i<arguments.length;i++){
+        this.result/=numbers[i];
+    }
+    return this;
+}
+// console.log(Math_lib);
+const math = new Math_lib();
+math.add(5, 10).multiply(6).subtract(10, 30, 8).divide(2);
 console.log(math.result);
